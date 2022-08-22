13466a48f6ca3ca72fef3f1f3bce5a0e04091976
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-22 10:27:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(defaultActivity= 'roller-skate') {
+    return (`This Saturday, I want to ${defaultActivity}!`);
+}
+
+saturdayFun()
+
+function mondayWork(defaultActivity= 'go to the office') {
+    return `This Monday, I will ${defaultActivity}.`;
+}
+mondayWork()
+
+let wrapAdjective = function(noun="*") {
+    return function(adjective) {
+      return `You are ${noun}${adjective}${noun}!`
+    }
+}
